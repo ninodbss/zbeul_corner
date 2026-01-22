27b674f877ebffccc19aa19452c1d56443e68f98
Nino DUBOIS <ninodubois@MacBook-Air-de-Valerie.local>
2026-01-22 22:17:10 +0100

    Allow remote avatars (Next Image)

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {}
+  experimental: {},
+
+  images: {
+    remotePatterns: [
+      // tes avatars de test
+      { protocol: "https", hostname: "i.pravatar.cc" },
+
+      // TikTok (au cas où tu utilises leur CDN plus tard)
+      { protocol: "https", hostname: "p16-sign-va.tiktokcdn.com" },
+      { protocol: "https", hostname: "p16-sign.tiktokcdn.com" },
+      { protocol: "https", hostname: "p19-sign.tiktokcdn-us.com" },
+      { protocol: "https", hostname: "p16-tiktokcdn.com" },
+    ],
+  },
 };
+
 export default nextConfig;
